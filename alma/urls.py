b2e535ed90cd61444944a69d90309364a920981a
Pablo <112183476+pabloscopeo@users.noreply.github.com>
2024-04-02 20:27:30 -0300

    added example route

--- alma/urls.py
+++ alma/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from api import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("hello/", views.HelloWorldView.as_view(), name="hello-world"),
 ]
