6e974d45ddd3a22295d8a21595c0ec79f3514be4
Pablo <pablo.skewes@ug.uchile.cl>
2024-04-11 18:40:53 -0400

    feat: imagenes carrusel
    - fix urls
    - added automatic docs
    - added model, serializer and view for image carrusel
    - added entity to admin page

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
+from django.utils.html import mark_safe
 
-# Register your models here.
+from .models import CarouselImage
+
+
+class CarouselImageAdmin(admin.ModelAdmin):
+    list_display = ("title", "creation_date")
+    search_fields = ("title", "description")
+    list_filter = ("creation_date",)
+    readonly_fields = ("image_tag",)
+
+    def image_tag(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="200" height="200" />')
+        return "No Image"
+
+    image_tag.short_description = "Image Preview"
+
+
+admin.site.register(CarouselImage, CarouselImageAdmin)
--- api/views.py
+++ api/views.py
@@ -1,7 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
+
+from .models import CarouselImage
+from .serializers import CarouselImageSerializer
 
 
 class HelloWorldView(APIView):
+    @extend_schema(tags=["API"])
     def get(self, request):
         return Response({"message": "Hello, World!"})
+
+
+class CarouselImageView(APIView):
+    @extend_schema(tags=["API"])
+    def get(self, request):
+        carousel_images = CarouselImage.objects.all()
+        serializer = CarouselImageSerializer(carousel_images, many=True)
+        return Response(serializer.data)
