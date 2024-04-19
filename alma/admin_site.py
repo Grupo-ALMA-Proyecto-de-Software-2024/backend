da085a0642e82cddca3e6d0384c084411b906180
Pablo <pablo.skewes@ug.uchile.cl>
2024-04-19 17:50:47 -0400

    feat: functions to sort apps

--- alma/admin_site.py
+++ alma/admin_site.py
@@ -1,35 +1,101 @@
-from typing import Any, List
+from typing import Any, List, Dict
 from django.contrib import admin
-from django.core.handlers.wsgi import WSGIRequest
 
 
-def sort_models_in_app_list(
-    apps: List[Any],
+def find_app(apps: Dict[str, Any], app_name: str) -> Any:
+    return next((app for app in apps.values() if app["name"] == app_name), None)
+
+
+def sort_apps(apps: Dict[str, Any], app_order: List[str]) -> Dict[str, Any]:
+    """
+    Sort the apps in the order specified by app_order.
+    Args:
+        apps: A dictionary of apps to sort, comes from self._build_app_dict (django)
+        app_order: A list of app names in the order they should be sorted.
+    Returns:
+        The same dictionary of apps, but sorted.
+    """
+    try:
+        sorted_apps = sorted(
+            apps.values(),
+            key=lambda app: app_order.index(app["name"]),
+        )
+    except ValueError as e:
+        if "not in list" in str(e):
+            print(f"{e}: {app_order}")
+            raise e
+        raise e
+    return sorted_apps
+
+
+def sort_models_within_app(
+    apps: Dict[str, Any],
     app_name: str,
     model_order: List[str],
-) -> List[Any]:
+) -> Dict[str, Any]:
     """
-    Sorts the models in the app list by the order of the model_order list.
-    If the app is not found, the app list is returned as is.
+    Sort the models within an app in the order specified by model_order.
     Args:
-        apps: The app list, as returned by the get_app_list method (django)
+        app: A dictionary of an app to sort, comes from self._build_app_dict (django)
         app_name: The name of the app to sort.
-        model_order: The order of the models wanted in the app.
+        model_order: A list of model names in the order they should be sorted.
+    Returns:
+        The same dictionary of an app, but with models sorted.
     """
-    app = next((app for app in apps if app["name"] == app_name), None)
-    if app:
-        app["models"] = sorted(app["models"], key=lambda x: model_order.index(x["object_name"]))
-        print(app["models"])
-    return apps
+    app = find_app(apps=apps, app_name=app_name)
+    try:
+        sorted_models = sorted(
+            app["models"],
+            key=lambda model: model_order.index(model["name"]),
+        )
+    except ValueError as e:
+        if "not in list" in str(e):
+            print(f"{e}: {model_order}")
+            raise e
+        raise e
+    return sorted_models
 
 
-order_of_api_models = [
-    "Region",
-    "Disk",
-    "Band",
-    "Molecule",
-    "CarouselImage",
+MODELS_TEMPLATE = [
+    {
+        "app_backend_name": "Api",
+        "name_to_display": "Recursos",
+        "models": [
+            "Regions",
+            "Disks",
+            "Bands",
+            "Molecules",
+            "Carousel images",
+        ],
+    },
+    {
+        "app_backend_name": "Authentication and Authorization",
+        "name_to_display": "Usuarios",
+        "models": [
+            "User",
+        ],
+    },
 ]
+
+
+def update_apps_organization(apps: Dict[str, Any], template: List[Dict[str, Any]]) -> Dict[str, Any]:
+    """
+    Update the organization of the apps and models in the admin site.
+    Args:
+        apps: A dictionary of apps to sort, comes from self._build_app_dict (django)
+        template: A list of dictionaries with the organization of the apps and models.
+    Returns:
+        The same dictionary of apps, but with the updated organization.
+    """
+    for app in template:
+        app_name = app["app_backend_name"]
+        models = app["models"]
+        sort_models_within_app(
+            apps=apps,
+            app_name=app_name,
+            model_order=models,
+        )
+    return apps
 
 
 class AlmaAdminSite(admin.AdminSite):
@@ -37,7 +103,32 @@ class AlmaAdminSite(admin.AdminSite):
     site_title = "ALMA Administration"
     index_title = "ALMA Administration"
 
-    def get_app_list(self, request: WSGIRequest) -> List[Any]:
-        apps = super().get_app_list(request)
-        apps = sort_models_in_app_list(apps, "API", order_of_api_models)
-        return apps
+    def get_app_list(self, request, app_label=None):
+        """
+        Return a sorted list of all the installed apps that have been
+        registered in this site.
+        """
+        app_dict = self._build_app_dict(request, app_label)
+
+        # Sort the apps alphabetically.
+        app_list = sort_apps(
+            apps=app_dict,
+            app_order=[
+                "Authentication and Authorization",
+                "Api",
+            ],
+        )
+
+        sort_models_within_app(
+            apps=app_dict,
+            app_name="Api",
+            model_order=[
+                "Regions",
+                "Disks",
+                "Bands",
+                "Molecules",
+                "Carousel images",
+            ],
+        )
+
+        return app_list
