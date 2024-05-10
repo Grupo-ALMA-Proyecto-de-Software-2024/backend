334666c24e15e7959c42ebcac23583cfa0c89c61
Pablo Skewes <pablo.skewes@ug.uchile.cl>
2024-05-09 20:00:34 -0400

    Support multiple query params on data services (#7)
    * support for multiple params
    
    * simplified model filters
    
    * fix lines too long
    
    * added tests to check filtering with existing and non existing values

--- api/models.py
+++ api/models.py
@@ -37,12 +37,16 @@ class Disk(BaseDataModel):
     region = models.ForeignKey(Region, on_delete=models.CASCADE, related_name="disks")
 
     @classmethod
-    def filter_disks(cls, name: Optional[str] = None, region: Optional[str] = None):
+    def filter_disks(
+        cls,
+        name: Optional[list[str]] = None,
+        region: Optional[list[str]] = None,
+    ):
         disks = cls.objects.select_related("region").all()
         if name:
-            disks = disks.filter(name=name)
+            disks = disks.filter(name__in=name)
         if region:
-            disks = disks.filter(region__name=region)
+            disks = disks.filter(region__name__in=region)
         return disks
 
 
@@ -54,17 +58,17 @@ class Band(BaseDataModel):
     @classmethod
     def filter_bands(
         cls,
-        name: Optional[str] = None,
-        disk: Optional[str] = None,
-        region: Optional[str] = None,
+        name: Optional[list[str]] = None,
+        disk: Optional[list[str]] = None,
+        region: Optional[list[str]] = None,
     ):
         bands = cls.objects.select_related("disk__region").all()
         if name:
-            bands = bands.filter(name=name)
+            bands = bands.filter(name__in=name)
         if disk:
-            bands = bands.filter(disk__name=disk)
+            bands = bands.filter(disk__name__in=disk)
         if region:
-            bands = bands.filter(disk__region__name=region)
+            bands = bands.filter(disk__region__name__in=region)
         return bands
 
 
@@ -76,20 +80,20 @@ class Molecule(BaseDataModel):
     @classmethod
     def filter_molecules(
         cls,
-        name: Optional[str] = None,
-        band: Optional[str] = None,
-        disk: Optional[str] = None,
-        region: Optional[str] = None,
+        name: Optional[list[str]] = None,
+        band: Optional[list[str]] = None,
+        disk: Optional[list[str]] = None,
+        region: Optional[list[str]] = None,
     ):
         molecules = cls.objects.select_related("band__disk__region").all()
         if name:
-            molecules = molecules.filter(name=name)
+            molecules = molecules.filter(name__in=name)
         if band:
-            molecules = molecules.filter(band__name=band)
+            molecules = molecules.filter(band__name__in=band)
         if disk:
-            molecules = molecules.filter(band__disk__name=disk)
+            molecules = molecules.filter(band__disk__name__in=disk)
         if region:
-            molecules = molecules.filter(band__disk__region__name=region)
+            molecules = molecules.filter(band__disk__region__name__in=region)
         return molecules
 
 
@@ -108,21 +112,21 @@ class Data(BaseDataModel):
     @classmethod
     def filter_data(
         cls,
-        name: Optional[str] = None,
-        molecule: Optional[str] = None,
-        band: Optional[str] = None,
-        disk: Optional[str] = None,
-        region: Optional[str] = None,
+        name: Optional[list[str]] = None,
+        molecule: Optional[list[str]] = None,
+        band: Optional[list[str]] = None,
+        disk: Optional[list[str]] = None,
+        region: Optional[list[str]] = None,
     ):
         data = cls.objects.select_related("molecule__band__disk__region").all()
         if name:
-            data = data.filter(name=name)
+            data = data.filter(name__in=name)
         if molecule:
-            data = data.filter(molecule__name=molecule)
+            data = data.filter(molecule__name__in=molecule)
         if band:
-            data = data.filter(molecule__band__name=band)
+            data = data.filter(molecule__band__name__in=band)
         if disk:
-            data = data.filter(molecule__band__disk__name=disk)
+            data = data.filter(molecule__band__disk__name__in=disk)
         if region:
-            data = data.filter(molecule__band__disk__region__name=region)
+            data = data.filter(molecule__band__disk__region__name__in=region)
         return data
--- api/tests/test_metadata_views.py
+++ api/tests/test_metadata_views.py
@@ -136,3 +136,201 @@ class ViewTestWithQueryParams(TestCase):
         self.assertIn("data", response.data)
         self.assertEqual(len(response.data["data"]), 1)
         self.assertEqual(response.data["data"][0]["name"], "Data1")
+
+
+class ViewTestWithMultipleFilters(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        self.region1 = Region.objects.create(name="Region1")
+        self.region2 = Region.objects.create(name="Region2")
+        self.disk1 = Disk.objects.create(name="Disk1", region=self.region1)
+        self.disk2 = Disk.objects.create(name="Disk2", region=self.region2)
+        self.band1 = Band.objects.create(name="Band1", disk=self.disk1)
+        self.band2 = Band.objects.create(name="Band2", disk=self.disk2)
+        self.molecule1 = Molecule.objects.create(name="Molecule1", band=self.band1)
+        self.molecule2 = Molecule.objects.create(name="Molecule2", band=self.band2)
+        self.data1 = Data.objects.create(
+            name="Data1", molecule=self.molecule1, file="file1", is_viewable=True
+        )
+        self.data2 = Data.objects.create(
+            name="Data2", molecule=self.molecule2, file="file2", is_viewable=True
+        )
+
+    def test_region_view_with_multiple_filters(self):
+        url = reverse("regions") + "?region=Region1&region=Region2"
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("regions", response.data)
+        self.assertEqual(len(response.data["regions"]), 2)
+        self.assertEqual(response.data["regions"][0]["name"], "Region1")
+        self.assertEqual(response.data["regions"][1]["name"], "Region2")
+
+    def test_disk_view_with_multiple_filters(self):
+        url = reverse("disks") + "?disk=Disk1&disk=Disk2&region=Region1&region=Region2"
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("disks", response.data)
+        self.assertEqual(len(response.data["disks"]), 2)
+        self.assertEqual(response.data["disks"][0]["name"], "Disk1")
+        self.assertEqual(response.data["disks"][1]["name"], "Disk2")
+
+    def test_band_view_with_multiple_filters(self):
+        url = (
+            reverse("bands")
+            + "?band=Band1&band=Band2&disk=Disk1&disk=Disk2&region=Region1&region=Region2"
+        )
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("bands", response.data)
+        self.assertEqual(len(response.data["bands"]), 2)
+        self.assertEqual(response.data["bands"][0]["name"], "Band1")
+        self.assertEqual(response.data["bands"][1]["name"], "Band2")
+
+    def test_molecule_view_with_multiple_filters(self):
+        url = (
+            reverse("molecules")
+            + "?molecule=Molecule1&molecule=Molecule2&band=Band1&band=Band2"
+            + "&disk=Disk1&disk=Disk2&region=Region1&region=Region2"
+        )
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("molecules", response.data)
+        self.assertEqual(len(response.data["molecules"]), 2)
+        self.assertEqual(response.data["molecules"][0]["name"], "Molecule1")
+        self.assertEqual(response.data["molecules"][1]["name"], "Molecule2")
+
+    def test_data_view_with_multiple_filters(self):
+        url = (
+            reverse("data")
+            + "?data=Data1&data=Data2&molecule=Molecule1&molecule=Molecule2"
+            + "&band=Band1&band=Band2&disk=Disk1&disk=Disk2&region=Region1&region=Region2"
+        )
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("data", response.data)
+        self.assertEqual(len(response.data["data"]), 2)
+        self.assertEqual(response.data["data"][0]["name"], "Data1")
+        self.assertEqual(response.data["data"][1]["name"], "Data2")
+
+    def test_region_view_with_filter_and_a_non_existent_region(self):
+        url = reverse("regions") + "?region=Region1&region=Region3"
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("regions", response.data)
+        self.assertEqual(len(response.data["regions"]), 1)
+        self.assertEqual(response.data["regions"][0]["name"], "Region1")
+
+    def test_disk_view_with_filter_and_a_non_existent_disk(self):
+        url = reverse("disks") + "?disk=Disk1&disk=Disk3&region=Region1"
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("disks", response.data)
+        self.assertEqual(len(response.data["disks"]), 1)
+        self.assertEqual(response.data["disks"][0]["name"], "Disk1")
+
+    def test_band_view_with_filter_and_a_non_existent_band(self):
+        url = reverse("bands") + "?band=Band1&band=Band3&disk=Disk1&region=Region1"
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("bands", response.data)
+        self.assertEqual(len(response.data["bands"]), 1)
+        self.assertEqual(response.data["bands"][0]["name"], "Band1")
+
+    def test_molecule_view_with_filter_and_a_non_existent_molecule(self):
+        url = (
+            reverse("molecules")
+            + "?molecule=Molecule1&molecule=Molecule3&band=Band1&disk=Disk1&region=Region1"
+        )
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("molecules", response.data)
+        self.assertEqual(len(response.data["molecules"]), 1)
+        self.assertEqual(response.data["molecules"][0]["name"], "Molecule1")
+
+    def test_data_view_with_filter_and_a_non_existent_data(self):
+        url = (
+            reverse("data")
+            + "?data=Data1&data=Data3&molecule=Molecule1&band=Band1&disk=Disk1&region=Region1"
+        )
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("data", response.data)
+        self.assertEqual(len(response.data["data"]), 1)
+        self.assertEqual(response.data["data"][0]["name"], "Data1")
+
+
+class ViewTestWithNoResults(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        self.region1 = Region.objects.create(name="Region1")
+        self.region2 = Region.objects.create(name="Region2")
+        self.disk1 = Disk.objects.create(name="Disk1", region=self.region1)
+        self.disk2 = Disk.objects.create(name="Disk2", region=self.region2)
+        self.band1 = Band.objects.create(name="Band1", disk=self.disk1)
+        self.band2 = Band.objects.create(name="Band2", disk=self.disk2)
+        self.molecule1 = Molecule.objects.create(name="Molecule1", band=self.band1)
+        self.molecule2 = Molecule.objects.create(name="Molecule2", band=self.band2)
+        self.data1 = Data.objects.create(
+            name="Data1", molecule=self.molecule1, file="file1", is_viewable=True
+        )
+        self.data2 = Data.objects.create(
+            name="Data2", molecule=self.molecule2, file="file2", is_viewable=True
+        )
+
+    def test_region_view_no_results(self):
+        url = reverse("regions") + "?region=Region3"
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("regions", response.data)
+        self.assertEqual(len(response.data["regions"]), 0)
+
+    def test_disk_view_no_results(self):
+        url = reverse("disks") + "?disk=Disk3&region=Region3"
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("disks", response.data)
+        self.assertEqual(len(response.data["disks"]), 0)
+
+    def test_band_view_no_results(self):
+        url = reverse("bands") + "?band=Band3&disk=Disk3&region=Region3"
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("bands", response.data)
+        self.assertEqual(len(response.data["bands"]), 0)
+
+    def test_molecule_view_no_results(self):
+        url = (
+            reverse("molecules")
+            + "?molecule=Molecule3&band=Band3&disk=Disk3&region=Region3"
+        )
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("molecules", response.data)
+        self.assertEqual(len(response.data["molecules"]), 0)
+
+    def test_data_view_no_results(self):
+        url = (
+            reverse("data")
+            + "?data=Data3&molecule=Molecule3&band=Band3&disk=Disk3&region=Region3"
+        )
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("data", response.data)
+        self.assertEqual(len(response.data["data"]), 0)
