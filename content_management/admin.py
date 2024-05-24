f0f426614f77b75e9cdd6d01692cfbda49cebe89
Pablo Skewes <pablo.skewes@ug.uchile.cl>
2024-05-24 01:04:35 -0400

    feat: added markdown viz in django admin (#10)
    * feat: added markdown viz in django admin
    
    * Update pyproject.toml

--- content_management/admin.py
+++ content_management/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.utils.html import mark_safe
 
+from .utils import markdown_to_html
+
 
 class CarouselImageAdmin(admin.ModelAdmin):
     list_display = ("title", "creation_date")
@@ -20,19 +22,20 @@ class CarouselImageAdmin(admin.ModelAdmin):
 class PublicationAdmin(admin.ModelAdmin):
     list_display = ("title", "authors", "journal_info")
     search_fields = ("title", "authors", "journal_info")
-    readonly_fields = ("image_tag",)
+    readonly_fields = ("image_tag", "summary_html")
     fields = (
         "title",
         "authors",
         "full_authors",
         "journal_info",
-        "summary",
-        "image",
-        "image_tag",
         "pdf_link",
         "bibtex_link",
         "data_link",
         "sao_nasa_link",
+        "image",
+        "image_tag",
+        "summary",
+        "summary_html",
     )
 
     def image_tag(self, obj):
@@ -40,4 +43,8 @@ class PublicationAdmin(admin.ModelAdmin):
             return mark_safe(f'<img src="{obj.image.url}" width="200" height="200" />')
         return "No Image"
 
+    def summary_html(self, obj):
+        return markdown_to_html(obj.summary)
+
     image_tag.short_description = "Image Preview"
+    summary_html.short_description = "Summary Visualization"
