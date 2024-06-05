d54f10b33fb392bac04f642353380f3e252a9711
Pablo <pablo.skewes@ug.uchile.cl>
2024-06-05 13:37:20 -0400

    simplified horzonal view

--- api/admin.py
+++ api/admin.py
@@ -1,40 +1,6 @@
 from django.contrib import admin
-from django import forms
-from django.contrib.admin.widgets import FilteredSelectMultiple
 
 from . import models
-
-
-class CustomFilteredSelectMultiple(FilteredSelectMultiple):
-    def __init__(self, verbose_name, is_stacked=False, *args, **kwargs):
-        super().__init__(verbose_name, is_stacked, *args, **kwargs)
-
-
-class DiskAdminForm(forms.ModelForm):
-    class Meta:
-        model = models.Disk
-        fields = "__all__"
-        widgets = {
-            "regions": CustomFilteredSelectMultiple("Regions", is_stacked=False),
-        }
-
-
-class BandAdminForm(forms.ModelForm):
-    class Meta:
-        model = models.Band
-        fields = "__all__"
-        widgets = {
-            "disks": CustomFilteredSelectMultiple("Disks", is_stacked=False),
-        }
-
-
-class MoleculeAdminForm(forms.ModelForm):
-    class Meta:
-        model = models.Molecule
-        fields = "__all__"
-        widgets = {
-            "bands": CustomFilteredSelectMultiple("Bands", is_stacked=False),
-        }
 
 
 # Inline tables
@@ -74,15 +40,15 @@ class RegionAdmin(admin.ModelAdmin):
 
 
 class DiskAdmin(admin.ModelAdmin):
-    form = DiskAdminForm
     inlines = (BandInline,)
+    filter_horizontal = ("regions",)
 
 
 class BandAdmin(admin.ModelAdmin):
-    form = BandAdminForm
     inlines = (MoleculeInline,)
+    filter_horizontal = ("disks",)
 
 
 class MoleculeAdmin(admin.ModelAdmin):
-    form = MoleculeAdminForm
     inlines = (DataInline,)
+    filter_horizontal = ("bands",)
