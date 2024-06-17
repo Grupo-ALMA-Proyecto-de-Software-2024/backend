2d4ff090815dfe2fabd25f9016a1adab42c0018b
Pablo <pablo.skewes@ug.uchile.cl>
2024-06-17 18:56:26 -0400

    first bulk upload idea

--- alma/admin.py
+++ alma/admin.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 
 from alma.admin_site import AlmaAdminSite
+from alma.bulk_upload_view import BulkUploadAdminView
 from api.models import Region, Disk, Band, Molecule, Data
 from api.admin import RegionAdmin, DiskAdmin, BandAdmin, MoleculeAdmin
 from content_management.models import CarouselImage, Publication, PressNews
@@ -15,6 +16,7 @@ admin_site = AlmaAdminSite(name="ALMA Administration")
 
 # core admin
 admin_site.register(User, UserAdmin)
+admin_site.register(BulkUploadAdminView)
 
 # api admin
 admin_site.register(Region, RegionAdmin)
